cc83856a2b855fa26423042761399d4b2308d323
Alvin Yuga Pramana <kifay0715@gmail.com>
2024-09-27 07:07:11 +0800

    Memperbaiki Codingan di file random_number.dart

--- random_number/lib/random_number.dart
+++ random_number/lib/random_number.dart
@@ -17,6 +17,8 @@ void main() {
   }
   print(numbers);
 
+  Stopwatch stopwatch = Stopwatch();
+
   print("====================");
   print(" CHOOSE NUMBERS 1-3 ");
   print("====================");
@@ -34,9 +36,12 @@ void main() {
       print('  BUBBLE-SORT  ');
       print('===============');
 
+      stopwatch.start();
       print('Before Bubble-Sort -> $numbers');
       bubblesort(numbers);
       print('After Bubble-Sort -> $numbers');
+      stopwatch.stop();
+      print('Time Bubble Sort : ${stopwatch.elapsedMilliseconds} ms');
       break;
 
     case '2':
@@ -44,9 +49,12 @@ void main() {
       print('    INVERSI    ');
       print('===============');
 
+      stopwatch.start();
       print('Before Inversi -> $numbers');
-      List<int> reverseNumbers = numbers.reversed.toList();
-      print('After Inversi -> $reverseNumbers');
+      List<int> inversiNumbers = numbers.reversed.toList();
+      print('After Inversi -> $inversiNumbers');
+      stopwatch.stop();
+      print('Time Inversi : ${stopwatch.elapsedMilliseconds} ms');
       break;
 
     case '3':
@@ -54,26 +62,35 @@ void main() {
       print('    SHUFFLE   ');
       print('==============');
 
+      stopwatch.start();
       print('Before Shuffle -> $numbers');
       numbers.shuffle();
       print('After Shuffle -> $numbers');
+            stopwatch.stop();
+      print('Time Shuffle : ${stopwatch.elapsedMilliseconds} ms');
       break;
 
     case '4':
       print('==============');
-      print('   PIRAMIDA   ');
+      print('    PYRAMID   ');
       print('==============');
 
+      stopwatch.start();
       piramid_while(numbers);
+      stopwatch.stop();
+      print('Time Pyramid : ${stopwatch.elapsedMilliseconds} ms');
       break;
 
     case '5':
       print('================');
-      print('PIRAMIDA BERURUT');
+      print('SEQUENCE PYRAMID');
       print('================');
 
+      stopwatch.start();
       bubblesort(numbers);
       piramid_while(numbers);
+      stopwatch.stop();
+      print('Time Sequence Pyramid : ${stopwatch.elapsedMilliseconds} ms');
       break;
   }
 }
